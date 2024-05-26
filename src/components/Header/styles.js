13753491b388ea63f1ebbc00940036b751fb69f0
Caio Notebook <caiosantosxp@gmail.com>
2024-05-26 17:57:43 -0300

    adiciona Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  grid-area: header;
+
+  height: 105px;
+  width: 100%;
+
+  border-bottom: 1px solid ${props => props.theme.COLORS.BACKGROUND_700};
+
+  display: flex;
+  justify-content: space-between;
+
+  padding: 0 80px;
+`
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      font-size: 14px;
+      color: ${props => props.theme.COLORS.GRAY_100}
+    }
+
+    strong {
+      font-size: 18px;
+      color: ${props => props.theme.COLORS.WHITE}
+    }
+  }
+`
+
+export const Logout = styled.button`
+  border: 0;
+  background: none;
+
+  > svg {
+    color: ${props => props.theme.COLORS.GRAY_100};
+    font-size: 36px;
+  }
+`
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import './styles.js'
 import { Container } from './styles.js'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+
 
 export function Details(){
   return (
     <Container>
-      <h1>Caio Santos</h1>
+      <Header />
+      <Button title='Voltar'/>
     </Container>
   )
 }
